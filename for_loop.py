970dbd36f92890d71b9a13abc07ce65bd95fe465
Peggy LAN <pegger6521@gmail.com>
2022-04-09 13:58:00 +0800

    exercise

---
+++ for_loop.py
@@ -0,0 +1,7 @@
+# for loop的第一個練習
+# 對每一個在students清單裡面的人say hi
+# Allen Tom Mayday JJ Jolin Jay Jam
+
+students = ['Allen', 'Tom', 'Mayday', 'JJ', 'Jolin', 'Jay', 'Jam']
+for student in students:
+	print('Hi', student)
